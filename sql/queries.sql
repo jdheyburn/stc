05cd6c68236dda85879540d9df69e2385a72032f
Joseph Heyburn <jdheyburn@gmail.com>
2021-05-05 17:14:23 +0100

    Sql updates

--- sql/queries.sql
+++ sql/queries.sql
@@ -37,8 +37,16 @@ SELECT `flow_id`,`origin_code`,`destination_code`,`route_code`,`direction`,`star
 
 -- Step 3: Get fares for the given flow ID
 
+-- I've listed both out here to see if there is any difference between the flow fares (doesn't look like it)
+
 -- EGR -> SNR
-SELECT fare.id,fare.flow_id,fare.fare,fare.ticket_code,ticket_type.description as ticket_description,ticket_type.tkt_class as ticket_class,ticket_type.tkt_type as ticket_type, fare.restriction_code FROM `fare` LEFT JOIN ticket_type on fare.ticket_code = ticket_type.ticket_code WHERE fare.flow_id IN (137711) AND ticket_type.start_date <= CURDATE() AND ticket_type.end_date > CURDATE()
+SELECT fare.id,fare.flow_id,fare.fare,fare.ticket_code,ticket_type.description as ticket_description,ticket_type.tkt_class as ticket_class,ticket_type.tkt_type as ticket_type,
+fare.restriction_code, restriction_header.description as restriction_desc, restriction_header.desc_out as restriction_desc_out, restriction_header.desc_ret as restriction_desc_rtn
+FROM `fare` 
+LEFT JOIN ticket_type on fare.ticket_code = ticket_type.ticket_code 
+LEFT JOIN restriction_header on fare.restriction_code = restriction_header.restriction_code 
+WHERE fare.flow_id IN (137711) AND ticket_type.start_date <= CURDATE() AND ticket_type.end_date > CURDATE()
+
 
 |id         |flow_id|fare    |ticket_code|ticket_description|ticket_class|ticket_type|restriction_code|restriction_desc              |restriction_desc_out                              |restriction_desc_rtn                              |
 |-----------|-------|--------|-----------|------------------|------------|-----------|----------------|------------------------------|--------------------------------------------------|--------------------------------------------------|
@@ -66,6 +74,46 @@ SELECT fare.id,fare.flow_id,fare.fare,fare.ticket_code,ticket_type.description a
 |1064488    |137711 |850     |SDS        |ANYTIME DAY S     |2           |S          |                |                              |                                                  |                                                  |
 |1064489    |137711 |400     |TKR        |CHILD FLTFARE R   |2           |R          |                |                              |                                                  |                                                  |
 |1064490    |137711 |400     |TKS        |CHILD FLTFARE S   |2           |S          |                |                              |                                                  |                                                  |
+
+-- SNR -> EGR
+SELECT fare.id,fare.flow_id,fare.fare,fare.ticket_code,ticket_type.description as ticket_description,ticket_type.tkt_class as ticket_class,ticket_type.tkt_type as ticket_type,
+fare.restriction_code, restriction_header.description as restriction_desc, restriction_header.desc_out as restriction_desc_out, restriction_header.desc_ret as restriction_desc_rtn
+FROM `fare` 
+LEFT JOIN ticket_type on fare.ticket_code = ticket_type.ticket_code 
+LEFT JOIN restriction_header on fare.restriction_code = restriction_header.restriction_code 
+WHERE fare.flow_id IN (136991) AND ticket_type.start_date <= CURDATE() AND ticket_type.end_date > CURDATE()
+
+|id         |flow_id|fare    |ticket_code|ticket_description|ticket_class|ticket_type|restriction_code|restriction_desc              |restriction_desc_out                              |restriction_desc_rtn                              |
+|-----------|-------|--------|-----------|------------------|------------|-----------|----------------|------------------------------|--------------------------------------------------|--------------------------------------------------|
+|1058087    |136991 |1430    |0AJ        |SMART FCR         |1           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058087    |136991 |1430    |0AJ        |SMART FCR         |1           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058088    |136991 |950     |0AK        |SMART CDR         |2           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058088    |136991 |950     |0AK        |SMART CDR         |2           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058095    |136991 |950     |CDR        |OFF-PEAK DAY R    |2           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058095    |136991 |950     |CDR        |OFF-PEAK DAY R    |2           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058096    |136991 |1430    |FCR        |OFF-PEAK DAY 1R   |1           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058096    |136991 |1430    |FCR        |OFF-PEAK DAY 1R   |1           |R          |B1              |OFF-PEAK                      |VALID AFTER 0929 MON-FRI                          |VALID AFTER 0929 MON-FRI                          |
+|1058083    |136991 |1770    |0AC        |SMART FDR         |1           |R          |                |                              |                                                  |                                                  |
+|1058084    |136991 |1320    |0AD        |SMART FDS         |1           |S          |                |                              |                                                  |                                                  |
+|1058085    |136991 |1180    |0AE        |SMART SDR         |2           |R          |                |                              |                                                  |                                                  |
+|1058086    |136991 |880     |0AF        |SMART SDS         |2           |S          |                |                              |                                                  |                                                  |
+|1058089    |136991 |5300    |0AQ        |SMART 7DS         |2           |N          |                |                              |                                                  |                                                  |
+|1058090    |136991 |7950    |0AR        |SMART 7DF         |1           |N          |                |                              |                                                  |                                                  |
+|1058091    |136991 |5300    |0AS        |SMART PSS         |2           |N          |                |                              |                                                  |                                                  |
+|1058092    |136991 |7950    |0AT        |SMART PSF         |1           |N          |                |                              |                                                  |                                                  |
+|1058093    |136991 |7950    |7DF        |SEVEN DAY   1ST   |1           |N          |                |                              |                                                  |                                                  |
+|1058094    |136991 |5300    |7DS        |SEVEN DAY   STD   |2           |N          |                |                              |                                                  |                                                  |
+|1058097    |136991 |1770    |FDR        |ANYTIME DAY 1R    |1           |R          |                |                              |                                                  |                                                  |
+|1058098    |136991 |1320    |FDS        |ANYTIME DAY 1S    |1           |S          |                |                              |                                                  |                                                  |
+|1058099    |136991 |1180    |SDR        |ANYTIME DAY R     |2           |R          |                |                              |                                                  |                                                  |
+|1058100    |136991 |880     |SDS        |ANYTIME DAY S     |2           |S          |                |                              |                                                  |                                                  |
+|1058101    |136991 |400     |TKR        |CHILD FLTFARE R   |2           |R          |                |                              |                                                  |                                                  |
+|1058102    |136991 |400     |TKS        |CHILD FLTFARE S   |2           |S          |                |                              |                                                  |                                                  |
+
+
+
+
+-- Season tickets are calculated from 7DS for 2nd class and 7DF for 1st class
 
 
 
